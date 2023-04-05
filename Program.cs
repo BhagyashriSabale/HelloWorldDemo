8eaed05b47531b5850f359b8c4f654b499978868
Bhagyashri Sabale <balisabale16@gmail.com>
2023-04-05 09:05:45 +0530

    [Bhagyashri] Adding Welcome message

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hello World");
+        }
+    }
+}
