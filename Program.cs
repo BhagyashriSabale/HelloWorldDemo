260b8dfe9fec0f3a464b6a3a0ece3bec7a11579c
BhagyashriSabale <balisabale16@gmail.com>
2023-04-13 12:09:09 +0530

    [Bhagyashri] oopsconcept1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HelloWorldDemo.Inheritance;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,28 +12,45 @@ namespace HelloWorldDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hello World");
-            Console.WriteLine("Code pushing by 2nd Developers");
-            Console.WriteLine("Modifying in local repository");
-            Console.WriteLine("Good Morning");
-            Console.WriteLine("Modifying in remote repo");
-            Console.WriteLine("Hello");
-            //object syntax
-            //Classname variable=new ClassName
-            Human human = new Human();
-            //display msh on output screen
-            Console.WriteLine("Please enter human name");
-            //reading input from output screen
-            //reading input from output screen
-            human.name = Console.ReadLine();
-            Console.WriteLine("please enter your height");
-            human.height = Convert.ToSingle(Console.ReadLine());
-            Console.WriteLine("please enter your weight");
-            human.weight = Convert.ToSingle(Console.ReadLine());
-            Console.WriteLine("please enter your age");
-            human.age = Convert.ToInt32(Console.ReadLine());
-            human.Speak();
-            human.HumanDetails(human.name, human.height, human.weight, human.age);
+            Console.WriteLine("Please choose 1program from below options");
+            Console.WriteLine("1.Human\n2.Inheritance");
+            int option=Convert.ToInt32(Console.ReadLine());
 
+            switch(option)
+            {
+                case 1:
+                    //object syntax
+                    //Classname variable=new ClassName
+                    Human human = new Human();
+                    //display msh on output screen
+                    Console.WriteLine("Please enter human name");
+                    //reading input from output screen
+                    //reading input from output screen
+                    human.name = Console.ReadLine();
+                    Console.WriteLine("please enter your height");
+                    human.height = Convert.ToSingle(Console.ReadLine());
+                    Console.WriteLine("please enter your weight");
+                    human.weight = Convert.ToSingle(Console.ReadLine());
+                    Console.WriteLine("please enter your age");
+                    human.age = Convert.ToInt32(Console.ReadLine());
+                    human.Speak();
+                    human.HumanDetails(human.name, human.height, human.weight, human.age);
+                    break;
+
+                 case 2:
+                    Car car = new Car();
+                    car.modelNumber = 2022;
+                    car.brand = "BMW";
+                    car.Start();
+                    break;
+
+                default:
+                    Console.WriteLine("Please choose program with given options ");
+                    break;
+            }
+            
+            
+            
         }
     }
 }
